aea32f42094266064f2bc36d7c43126d9462a79d
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-16 17:05:51 +0100

    script.js and style.css was modified and img folder was created with images

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+let timer;
+const myImages = [
+    {
+        "img": "/img/400-800x800.jpg",
+        "caption": "image 1"
+    },
+    {
+        "img": "/img/429-800x800.jpg",
+        "caption": "image 2"
+    },
+    {
+        "img": "/img/431-800x800.jpg",
+        "caption": "image 3"
+    },
+    
+];
+
+builder();
+
+function builder(){
+    console.log(myImages);
+}
