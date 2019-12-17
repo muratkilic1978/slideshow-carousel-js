361820684b483681a2c9b335ea113e18eda9d2c2
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-17 16:53:37 +0100

    script.js -style.css and index.html files were modified

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-let slideIndex = 0;
-let timer;
-const myImages = [
-    {
-        "img": "/img/400-800x800.jpg",
-        "caption": "image 1"
-    },
-    {
-        "img": "/img/429-800x800.jpg",
-        "caption": "image 2"
-    },
-    {
-        "img": "/img/431-800x800.jpg",
-        "caption": "image 3"
-    },
+    let slideIndex = 0;
+    let timer;
+    const myImages = [
+        {
+            "img": "https://via.placeholder.com/250/09f?text=image1"
+            , "caption": "Hello World Image 1"
+    }
+        , {
+            "img": "https://via.placeholder.com/250/e4e?text=image2"
+            , "caption": "Hello World Image 2"
+    }
+        , {
+            "img": "https://via.placeholder.com/250/1f1?text=image3"
+            , "caption": "Hello World Image 3"
+    }
+ ];
     
-];
+    builder();
 
-builder();
+    function builder() {
+        for (let x = 0; x < myImages.length; x++) {
+            let slide = document.createElement("div");
+            slide.setAttribute("class", "mySlide fade");
+            let img = document.createElement("img");
+            img.setAttribute("src", myImages[x].img);
+            let cap = document.createElement("div");
+            cap.classList.add("caption");
+            cap.innerText = myImages[x].caption;
+            slide.appendChild(img);
+            slide.appendChild(cap);
+            document.querySelector(".slideContainer").appendChild(slide);
 
-function builder(){
-    console.log(myImages);
-}
+        }
+        
+    }
+
+   
