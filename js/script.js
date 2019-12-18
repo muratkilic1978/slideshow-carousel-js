e94455cdefa9901d340a4368cb9eff56c9ba4689
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-18 16:21:59 +0100

    script.js was modified

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-    let slideIndex = 0;
+let slideIndex = 0;
     let timer;
     const myImages = [
         {
-            "img": "https://via.placeholder.com/250/09f?text=image1"
+            "img": "http://localhost/slideshow-carousel-js/img/400-800x800.jpg"
             , "caption": "Hello World Image 1"
     }
         , {
-            "img": "https://via.placeholder.com/250/e4e?text=image2"
+            "img": "http://localhost/slideshow-carousel-js/img/429-800x800.jpg"
             , "caption": "Hello World Image 2"
     }
         , {
-            "img": "https://via.placeholder.com/250/1f1?text=image3"
+            "img": "http://localhost/slideshow-carousel-js/img/431-800x800.jpg"
             , "caption": "Hello World Image 3"
     }
  ];
-    
     builder();
 
     function builder() {
@@ -29,14 +28,38 @@
             slide.appendChild(img);
             slide.appendChild(cap);
             document.querySelector(".slideContainer").appendChild(slide);
+            console.log(slide);
             let span = document.createElement("span");
             span.classList.add("dot");
-            span.addEventListener("click",function(){
+            span.addEventListener("click", function () {
                 moveSlide(x);
             })
             document.querySelector(".indicator").appendChild(span);
         }
-        
+        playSlides();
     }
 
-   
+    function playSlides() {
+        const slides = document.querySelectorAll(".mySlide");
+        const dots = document.querySelectorAll(".dot");
+        const active = document.querySelector(".active");
+        //console.log(slides);
+        //console.log(dots);
+        //console.log(active);
+        if (slideIndex + 1 > slides.length) {
+            slideIndex = 0;
+        }
+        slides.forEach(function (el) {
+            el.style.display = "none";
+        })
+        slides[slideIndex].style.display = "block";
+        slideIndex++;
+        console.log(slideIndex);
+        timer = setTimeout(playSlides, 3000);
+    }
+
+    function moveSlide(num) {
+        slideIndex = num;
+        clearTimeout(timer);
+        playSlides();
+    }
